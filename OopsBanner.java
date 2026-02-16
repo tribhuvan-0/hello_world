2d76fe8499f2fc14a11ade3cfae0e19f09c0bd67
Godofthunder1121 <tribhuvang1088@gmail.com>
2026-02-16 21:39:16 +0530

    working code

--- OopsBanner.java
+++ OopsBanner.java
@@ -1,19 +1,12 @@
 public class OopsBanner {
-    public static void main(String[] args) {
-        String l1 = "  *** *** **** **** ";
-        String l2 = " * * * * * * * ";
-        String l3 = " * * * * * * * ";
-        String l4 = " * * * * **** **** ";
-        String l5 = " * * * * * *";
-        String l6 = " * * * * * *";
-        String l7 = "  *** *** * **** ";
+     public static void main(String[] args) {
 
-        System.out.println(l1);
-        System.out.println(l2);
-        System.out.println(l3);
-        System.out.println(l4);
-        System.out.println(l5);
-        System.out.println(l6);
-        System.out.println(l7);
-    }
+        System.out.println("  *****    *****    *****    ***** ");
+        System.out.println(" *     *  *     *  *     *  *     ");
+        System.out.println(" *     *  *     *  *     *  *     ");
+        System.out.println(" *     *  *     *  *****    ***** ");
+        System.out.println(" *     *  *     *  *            *  ");
+        System.out.println(" *     *  *     *  *            *  ");
+        System.out.println("  *****    *****   *      ******");
+}
 }
