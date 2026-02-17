8e96617bd58f0344b1315b90fc5997b2d1090b02
Godofthunder1121 <tribhuvang1088@gmail.com>
2026-02-17 13:51:37 +0530

    commiting uc4

---
+++ uc4.java
@@ -0,0 +1,14 @@
+public class uc4 {
+    public static void main(String[] args) {
+        String[] bannerLines = {
+            "  OOO    OOO   PPPP   SSSS ",
+            " O   O  O   O  P   P  S    ",
+            " O   O  O   O  PPPP    SSS ",
+            " O   O  O   O  P          S",
+            "  OOO    OOO   P      SSSS "
+        };
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
+    }
+}
